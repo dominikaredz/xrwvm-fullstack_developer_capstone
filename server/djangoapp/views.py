394df9e26147abe3afbdd6fd370fcceab5b42234
dominikaredz <dominika.redzko@gmail.com>
2026-09-03 12:08:33 -0400

    commit

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -8,6 +8,9 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.csrf import csrf_exempt
+from .models import CarMake, CarModel
+from .populate import initiate
+
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -86,3 +89,14 @@ def registration(request):
 # Create a `add_review` view to submit a review
 # def add_review(request):
 # ...
+
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
